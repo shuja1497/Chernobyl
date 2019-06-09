1bae5868cab55fc4d50b0bbf94e4c785922e3a55
Shuja Reshi <shujareshi1497@gmail.com>
2019-06-09 21:10:44 +0530

    project setup done for button click app

---
+++ ButtonClickApp/app/src/main/java/shuja1497/buttonclickapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package shuja1497.buttonclickapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
