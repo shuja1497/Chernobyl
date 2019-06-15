d185f586636a4fe9a474717e2850269bc248e680
Shuja Reshi <shujareshi1497@gmail.com>
2019-06-16 00:56:48 +0530

    basic functionality added for button click app

--- ButtonClickApp/app/src/main/java/shuja1497/buttonclickapp/MainActivity.kt
+++ ButtonClickApp/app/src/main/java/shuja1497/buttonclickapp/MainActivity.kt
@@ -2,11 +2,33 @@ package shuja1497.buttonclickapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userInput: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod() // to enable scrolling in textView
+
+        userInput.setText("")
+        button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                textView?.append(userInput.text)
+                textView?.append("\n")
+                userInput.setText("")
+            }
+        })
     }
 }
