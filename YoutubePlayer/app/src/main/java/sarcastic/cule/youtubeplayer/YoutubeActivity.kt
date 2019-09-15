93228dec391497810653c87a853710594548fd7d
shuja1497 <shujareshi1497@gmail.com>
2019-09-15 23:14:31 +0530

    added widget dynamically

--- YoutubePlayer/app/src/main/java/sarcastic/cule/youtubeplayer/YoutubeActivity.kt
+++ YoutubePlayer/app/src/main/java/sarcastic/cule/youtubeplayer/YoutubeActivity.kt
@@ -1,9 +1,13 @@
 package sarcastic.cule.youtubeplayer
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
 class YoutubeActivity : YouTubeBaseActivity() , YouTubePlayer.OnInitializedListener{
     override fun onInitializationSuccess(
@@ -21,6 +25,23 @@ class YoutubeActivity : YouTubeBaseActivity() , YouTubePlayer.OnInitializedListe
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+//        setContentView(R.layout.activity_youtube)
+//        val layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
+
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
+        setContentView(layout)
+
+//        val button = Button(this)
+//        button.layoutParams = ConstraintLayout.LayoutParams(600, 200)
+//        button.text = "Button Text"
+//        layout.addView(button)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        layout.addView(playerView)
+
     }
 }
